c73bdda8330353b3decf59c4797aecf33e10ae31
Ivan-alt2 <2011bor6@gmail.com>
2026-09-16 16:30:46 +0900

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,12 +1,26 @@
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from blog.models import Post, Category
+
+from blog.models import Category, Post
 
 
-def get_published_posts(posts: QuerySet = Post.objects.all()):
+# Константа для количества постов на странице
+POSTS_PER_PAGE = 5
+
+
+def get_published_posts(posts: QuerySet | None = None) -> QuerySet:
+    """
+    Возвращает опубликованные посты с оптимизированными запросами.
+    Если posts не передан, использует Post.objects.all().
+    """
+    if posts is None:
+        posts = Post.objects.all()
+
     return posts.select_related(
-        'category', 'location', 'author'
+        'category',
+        'location',
+        'author'
     ).filter(
         pub_date__lt=timezone.now(),
         is_published=True,
@@ -15,24 +29,26 @@ def get_published_posts(posts: QuerySet = Post.objects.all()):
 
 
 def index(request):
-    return render(
-        request, 'blog/index.html',
-        {'posts': get_published_posts()[:5]})
+    posts = get_published_posts()[:POSTS_PER_PAGE]
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def post_detail(request, post_id):
-    return render(
-        request, 'blog/detail.html',
-        {'post': get_object_or_404(get_published_posts(), pk=post_id)})
+    post = get_object_or_404(get_published_posts(), pk=post_id)
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
     category = get_object_or_404(
         Category,
         slug=category_slug,
-        is_published=True)
-    return render(request, 'blog/category.html', {
-        'category': category,
-        'posts': get_published_posts(category.post)})
+        is_published=True
+    )
+    # Используем обратную связь posts (а не post), так как в модели Post related_name='posts'
+    posts = get_published_posts(category.posts.all())
+    return render(
+        request,
+        'blog/category.html',
+        {'category': category, 'posts': posts}
+    )
 
-#        'posts': get_published_posts(Post.objects.filter(category=category))})
